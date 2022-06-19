37365ca8147cf16bcb220c55cc223ff6399f51f7
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-19 15:20:43 +0100

    improve variable naming and use clearer computation

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -24,14 +24,19 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
     let i = 1;
     let j = 1;
 
+    let smallFibonacciNumber = 1;
+    let largeFibonacciNumber = 1;
+
     let currentPosition = 2;
-    while (currentPosition < notNullPosition) {
-        const temp = i;
-        i = j;
-        j += temp;
+    while (currentPosition < position) {
+        const nextFibonacciNumber = smallFibonacciNumber + largeFibonacciNumber;
+        smallFibonacciNumber = largeFibonacciNumber;
+        largeFibonacciNumber = nextFibonacciNumber;
+        currentPosition++;
+
         currentPosition++;
     }
-    return j;
+    return largeFibonacciNumber;
 };
 
 const computeNegativeFibonacci = (position: number): number => {
@@ -46,18 +51,6 @@ const computeNegativeFibonacci = (position: number): number => {
 export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
     const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
     return inputArray.map(x => computeFibonacciNumber(x));
-}
-
-const recursiveFibonacci = (previous: number, current: number, stepsLeft: number): number => {
-    if (stepsLeft < 0) {
-        return 1;
-    }
-    switch (stepsLeft) {
-        case 0:
-            return current;
-        default:
-            return recursiveFibonacci(current, previous + current, stepsLeft - 1);
-    }
 }
 
 const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
